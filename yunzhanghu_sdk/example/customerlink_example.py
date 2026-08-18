06930687a70994342df9b14ec48afbdc81e4efff
kerun.wu <kerun.wu@yunzhanghu.com>
2026-08-18 03:39:45 +0000

    调整注释"客服链接"为"专属服务链接"

--- yunzhanghu_sdk/example/customerlink_example.py
+++ yunzhanghu_sdk/example/customerlink_example.py
@@ -3,7 +3,7 @@
 from yunzhanghu_sdk.example.utils.config_init import init_config
 from yunzhanghu_sdk.utils import Utils
 
-# 生成最终客服链接
+# 生成专属服务链接
 if __name__ == "__main__":
     conf = init_config()
     ret = Utils.get_customer_link(conf, "https://www.example.com", 'testmemberid')
